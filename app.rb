8291baaaf2ee3f4bc558991d0a83164f97b37340
ushiba <ushiba@softbank126114114215.bbtec.net>
2010-06-20 11:45:57 +0900

    clean directories

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 require 'rubygems'
-require 'sinatra'
 require 'haml'
+require 'sass'
+require 'sinatra'
+require 'sinatra_more/markup_plugin'
+Sinatra::Base.register SinatraMore::MarkupPlugin
 
 get '/' do
   haml :index
+end
+
+get '/stylesheets/:stylesheet.css' do
+  content_type "text/css", :charset => "UTF-8"
+  sass :"sass/#{params[:stylesheet]}"
+end
+
+helpers do
+  def write_title
+    title = (@title || 'something fun') + ' @cowplace'
+  end
 end
