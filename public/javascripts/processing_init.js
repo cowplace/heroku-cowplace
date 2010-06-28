febba534f33851b5bc4489998af9fc9fdd8abe54
ushiba <ushiba@softbank126126100028.bbtec.net>
2010-06-28 22:57:13 +0900

    modified multi particles2

--- public/javascripts/processing_init.js
+++ public/javascripts/processing_init.js
@@ -1,10 +1,11 @@
 function init() {
   var canvas = $('canvas');
-
-  // Processing のソースコードが書かれた script 要素
   var codeElm = $('processing-code');
-  // 要素の内容を取得する
-  var code = codeElm.textContent || codeElm.innerText;
+  var code = Try.these(
+      function(){return codeElm.textContent},
+      function(){return codeElm.innerText},
+      function(){return codeElm.text}
+  );
 
   Processing(canvas, code);
 }
