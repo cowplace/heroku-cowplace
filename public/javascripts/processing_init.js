63ecc92d1053adf01678b10a8eb4e79dafbd2cc5
ushiba <ushiba@softbank126114116199.bbtec.net>
2010-06-29 23:09:40 +0900

    modified multi particles3

--- public/javascripts/processing_init.js
+++ public/javascripts/processing_init.js
@@ -1,11 +1,7 @@
 function init() {
   var canvas = $('canvas');
   var codeElm = $('processing-code');
-  var code = Try.these(
-      function(){return codeElm.textContent},
-      function(){return codeElm.innerText},
-      function(){return codeElm.text}
-  );
+  var code = codeElm.text;
 
   Processing(canvas, code);
 }
