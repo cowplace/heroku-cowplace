df1f4705ae18436cb1159e8711d1dae05de6f72c
ushiba <ushiba@softbank126126144254.bbtec.net>
2010-06-30 20:29:13 +0900

    modified multi particles

--- public/javascripts/processing_init.js
+++ public/javascripts/processing_init.js
@@ -1,6 +1,5 @@
 function init(code_id) {
-  var stage = $('canvas_field');
-  stage.innerHTML = '<canvas id="canvas" width="800" height="400" />';
+  $('stage').innerHTML = '<canvas id="canvas" width="800" height="400" />';
   var canvas = $('canvas');
   var codeElm = $(code_id);
   var code = codeElm.text;
