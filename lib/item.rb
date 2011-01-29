aaaa8b2d73b5b4e30d6d2650653c983179334916
ushiba <ushiba@ushinote.local>
2011-01-29 23:19:52 +0900

    add items

--- lib/item.rb
+++ lib/item.rb
@@ -12,9 +12,11 @@ class Item
   class << self
     def get_items
       items = []
-      xml = REXML::Document.new(open('http://api.rakuten.co.jp/rws/3.0/rest?developerId=c9e2d430e9844443674e9cc80c63845a&affiliateId=0d65000a.224c5bad.0d65000b.0995e2dd&operation=BooksBookSearch&version=2011-01-27&publisherName=%e3%82%aa%e3%83%a9%e3%82%a4%e3%83%aa%e3%83%bc&sort=sales').read)
-      xml.elements.each('//Items/Item') do |item|
-        items << Item.new(item)
+      (1..4).each do |idx|
+        xml = REXML::Document.new(open("http://api.rakuten.co.jp/rws/3.0/rest?developerId=c9e2d430e9844443674e9cc80c63845a&affiliateId=0d65000a.224c5bad.0d65000b.0995e2dd&operation=BooksBookSearch&version=2011-01-27&publisherName=%e3%82%aa%e3%83%a9%e3%82%a4%e3%83%aa%e3%83%bc&page=#{idx}&sort=sales").read)
+        xml.elements.each('//Items/Item') do |item|
+          items << Item.new(item)
+        end
       end
       return items
     end
